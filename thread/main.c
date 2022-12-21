605feb53f691b66c13861795a5a151550e063812
Meng <1647452116@qq.com>
2022-12-21 10:47:55 +0800

    删除最上层 thread 文件夹

--- thread/main.c
+++
@@ -1,8 +0,0 @@
-﻿#include <stdio.h>
-
-int main()
-{
-    printf("%s 向你问好!\n", "Linux_thread_study");
-    pause();
-    return 0;
-}
--- thread/main.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include <cstdio>
-
-int main()
-{
-    printf("%s 向你问好!\n", "Linux_thread_study");
-    return 0;
-}
